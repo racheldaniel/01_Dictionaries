7618c4ebbe093775a666f083e3c9bb8ebfc8f20f
Rachel Daniel <rachdaniel88@gmail.com>
2019-01-09 12:27:39 -0600

    completed chapter one exercises

---
+++ stocks.py
@@ -0,0 +1,32 @@
+stock_dict = {'AAPL': 'Apple', 'AMZN': 'Amazon',
+              'NFLX': 'Netflix', 'GOOG': 'Google'}
+
+purchases = [('AAPL', 25, '04-oct-2006', 50), ('GOOG', 15, '01-dec-2016', 100), ('AMZN', 10, '20-mar-2010', 89),
+             ('NFLX', 10, '30-may-2018', 100), ('NFLX', 7, '13-jun-2016', 100), ('GOOG', 5, '01-jan-2019', 150)]
+
+# Create a purchase history report that computes the full purchase price (shares times dollars) for each block of stock and uses the stockDict to look up the full company name.
+
+for purchase in purchases:
+    print('I purchased {0} stock for ${1}'.format(
+        stock_dict[purchase[0]], purchase[3]))
+
+# Create a second purchase summary that which accumulates total investment by ticker symbol. In the above sample data, there are two blocks of GE.
+
+new_dict = {}
+for purchase in purchases:
+  key = purchase[0]
+  if key in new_dict:
+    new_dict[key].append(purchase)
+  else:
+    new_dict[key] = [purchase]
+
+print(new_dict)
+
+for company in new_dict.values():
+  price = 0
+  print('----{0}----'.format(company))
+  for block in company:
+    price += block[3]
+    print(block)
+  print('Total value of stock in portfolio: {0}'.format(price))
+
